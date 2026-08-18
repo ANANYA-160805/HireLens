234da93acef491af69246b2e981bd93c8daa89b5
ANANYA160805 <ananyasinha1599@outlook.com>
2026-08-18 13:26:50 +0530

    .

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,6 +8,13 @@ const app = express();
 
 app.use(express.json());
 
+/* require all the routes */
+const authRouter = require('./routes/auth.routes');
+
+
+/* use all the routes */
+app.use('/api/auth', authRouter);
+
 
 
 module.exports = app;
