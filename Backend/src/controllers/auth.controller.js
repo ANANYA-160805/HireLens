3cf321d399a5f0c7d77e0e9f011a042e80596003
ANANYA160805 <ananyasinha1599@outlook.com>
2026-08-21 23:43:10 +0530

    authlogin

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -50,10 +50,49 @@ async function registerUserController(req, res) {
             username: user.username,
             email: user.email,
         },
-        token
+      
     });
 }
 
+
+/**
+ * @name loginUserController
+ * @description login a user, expects email and password in the request body, verifies the credentials, and returns a JWT token if successful.
+ * @access Public
+ */
+
+async function loginUserController(req, res) {
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email});
+
+    if(!user) {
+        return res.status(400).json({ message: 'Invalid credentials' });
+    }
+
+    const isPasswordVaild = await bycrpt.compare(password, user.password);
+
+    if(!isPasswordVaild){
+        res.status(400).json({message : ' Invalid credentials'});
+    }
+
+    const token = jwt.sign({
+        id: user._id,
+        username: user.username,
+    }, process.env.JWT_SECRET,{expiresIn: '1d'})
+
+    res.cookie('token', token)
+
+    res.status(200).json({
+        message : 'User logged in successfully',
+        user:{
+            id : user._id,
+            username : user.username,
+            email : user.email
+        }
+    })
+}
 module.exports = {
-    registerUserController
+    registerUserController,
+    loginUserController
 };
--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -12,4 +12,13 @@ const authRouter = Router();
 
 authRouter.post('/register', authController.registerUserController);
 
+/**
+ * @route POST/api/auth/login
+ * @description login user with email and password
+ * @access Public
+ */
+
+
+authRouter.post('/api/login', authController.loginUserController);
+
 module.exports = authRouter;
